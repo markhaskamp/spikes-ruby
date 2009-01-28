7ebc2b739f5b49accb9c0e357cb33dc0b36c2a3f
Mark Haskamp <mrhask@gmail.com>
2009-01-27 22:13:58 -0500

    Added before call to tests.

--- strategy/Traveller.spec.rb
+++ strategy/Traveller.spec.rb
@@ -1,49 +1,57 @@
 require 'Travellers_All.rb'
 
 describe Traveller_Cowboy do
+  before(:each) do
+    @c = Traveller.create(:cowboy)
+  end
+
   it "When cowboy travels, then he 'moseys'" do
-    c = Traveller.create(:cowboy)
-    c.go.should eql("mosey")
+    @c.go.should eql("mosey")
   end
 
   it "When cowboy says hello, then he says 'howdy'" do
-    c = Traveller.create(:cowboy)
-    c.hello.should eql("howdy")
+    @c.hello.should eql("howdy")
   end
 end
 
 describe Traveller_Horse do
+  before(:each) do
+    @t = Traveller.create(:horse)
+  end
+
   it "When horse travels, then he 'gallops'" do
-    t = Traveller.create(:horse)
-    t.go.should eql("gallop")
+    @t.go.should eql("gallop")
   end
 
   it "When horse says hello, then he says 'animals don''t talk'" do
-    t = Traveller.create(:horse)
-    t.hello.should eql("animals don't talk")
+    @t.hello.should eql("animals don't talk")
   end
 end
 
 describe Traveller_Snake do
+  before(:each) do
+    @t = Traveller.create(:snake)
+  end
+
   it "When snake travels, then he 'slithers'" do
-    t = Traveller.create(:snake)
-    t.go.should eql("slither")
+    @t.go.should eql("slither")
   end
 
   it "When snake says hello, then he says 'animals don''t talk'" do
-    t = Traveller.create(:snake)
-    t.hello.should eql("animals don't talk")
+    @t.hello.should eql("animals don't talk")
   end
 end
 
 describe Traveller_Girl do
+  before(:each) do
+    @t = Traveller.create(:girl)
+  end
+
   it "When girl travels, then she 'skips'" do
-    t = Traveller.create(:girl)
-    t.go.should eql("skip")
+    @t.go.should eql("skip")
   end
 
   it "When girl says hello, then she says 'howdy'" do
-    t = Traveller.create(:girl)
-    t.hello.should eql("howdy")
+    @t.hello.should eql("howdy")
   end
 end
